fdae2995d9679dce9d3f3161fb39d2f8678ab24b
Diego Collares <diego.collares@gmail.com>
2016-08-29 15:59:09 -0300

    create scope.contacts

--- public/js/controllers/ContactsController.js
+++ public/js/controllers/ContactsController.js
@@ -1,7 +1,30 @@
 contatooh.controller('ContactsController'
 	, function($scope) {
+
+		$scope.contacts = [
+			{
+				"_id": 1,
+				"name": "Diego Collares",
+				"email": "diego.collares@gmail.com"
+			},
+			{
+				"_id": 2,
+				"name": "Laís Collares",
+				"email": "lais.collares@gmail.com"
+			},
+
+			{
+				"_id": 3,
+				"name": "Mariane Collares",
+				"email": "mariane.collares@gmail.com"
+			}
+
+		];
+
 		$scope.total = 0;
 		$scope.increment = function(){
 			$scope.total++;
 		};
+		$scope.filterName = "";
+
 });
