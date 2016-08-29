d2c236fca6d4f8791047bc87b7ca52442dccacf2
Diego Collares <diego.collares@gmail.com>
2016-08-29 00:31:13 -0300

    action to list Contacts and get contact

---
+++ app/controllers/contact.js
@@ -0,0 +1,28 @@
+var contacts = [
+	{id: 1, nome: 'Diego Collares', email: 'diego.collares@gmail.com'},
+	{id: 2, nome: 'Mariane Collares', email: 'mariane.collares@gmail.com'},
+	{id: 3, nome: 'Laís Collares', email: 'Laís.collares@gmail.com'},
+];
+
+module.exports = function(){
+	var controller = {};
+
+	controller.listContacts = function(req, res){
+		res.json(contacts);
+	};
+
+	controller.getContact = function(req, res){
+		var idContact	= req.params.id;
+		console.log(req.params.id);
+		var contact = contacts.filter(function(contact){
+			return contact.id == idContact;
+		})[0];
+
+		contact ?	
+			res.json(contact) :
+			res.status(404).send('Contato não encontrado');
+	}
+
+	return controller
+
+};
